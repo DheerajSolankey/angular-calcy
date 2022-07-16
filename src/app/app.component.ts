9646c0f3d0a885bf7d42808b0e2a1a2dac02d347
DheerajSolankey <dheeraj241099@gmail.com>
2022-07-16 16:11:37 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  title = 'calculator';
+  toshow='0'
+  currvalue=''
+
+
+  writetoinput(value:string)
+  {   if(this.currvalue=='0'){
+      this.currvalue=value
+    }
+    if(this.currvalue!='0')
+    {this.currvalue=this.currvalue+value
+    }
+    this.toshow = this.currvalue
+  }
+
+equals(){
+  this.toshow=eval(this.currvalue)
+  this.currvalue=this.toshow
+}
+
+clear(){
+  this.currvalue='';
+  this.toshow='0'
+}
+back(){
+  this.currvalue=this.currvalue.slice(0,-1)
+  if(this.currvalue==''){this.currvalue='0'}
+  this.toshow=this.currvalue
+}
+
+
+
+
+}
